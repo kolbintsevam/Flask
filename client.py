9bc081b8d6ffdca3b3696f352fd110d20abffb5a
Mariya Kolbintseva <kolbintsevam@mail.ru>
2023-07-07 02:01:51 +0300

    client.py

---
+++ client.py
@@ -0,0 +1,35 @@
+import requests
+
+# response = requests.post('http://127.0.0.1:5000/user', json={"name": 'user_6', 'password': 'rexroou5464556'})
+# print(response.status_code)
+# print(response.text)
+
+# response = requests.get('http://127.0.0.1:5000/user/1')
+# print(response.status_code)
+# print(response.text)
+
+
+# response = requests.patch('http://127.0.0.1:5000/user/7', json={"name": "new_user_name"})
+# print(response.status_code)
+# print(response.text)
+
+# response = requests.delete("http://127.0.0.1:5000/user/7")
+# print(response.status_code)
+# print(response.text)
+
+
+# response = requests.get("http://127.0.0.1:5000/ads/2")
+# print(response.status_code)
+# print(response.text)
+
+# response = requests.post('http://127.0.0.1:5000/ads', headers={"name": 'user_6', 'password': 'rexroou5464556'}, json={"title": 'my_ads', 'description': 'My Best Ads'})
+# print(response.status_code)
+# print(response.text)
+
+# response = requests.patch('http://127.0.0.1:5000/ads/48', headers={"name": 'user_4', 'password': '123gwesgtewt544'}, json={"title": 'funny_ads', 'description': 'My Best Ads'})
+# print(response.status_code)
+# print(response.text)
+
+# response = requests.delete("http://127.0.0.1:5000/ads/16", headers={"name": 'user_4', 'password': '123gwesgtewt544'})
+# print(response.status_code)
+# print(response.text)
